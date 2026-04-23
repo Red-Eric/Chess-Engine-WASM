00fb7618b85c45bbebbc7631ac5e4643e2e4d2a5
Red-Eric <redson.eric.2004@gmail.com>
2026-04-23 15:04:40 +0300

    fix

--- torch.js
+++ torch.js
@@ -1,17 +1,3 @@
-function base64ToArrayBuffer(base64) {
-  const binary = atob(base64);
-  const len = binary.length;
-  const bytes = new Uint8Array(len);
-
-  for (let i = 0; i < len; i++) {
-    bytes[i] = binary.charCodeAt(i);
-  }
-
-  return bytes.buffer;
-}
-
-
-
 var Module = typeof Module != "undefined" ? Module : {};
 var KOMODO_TEP = function() {
         function loadKomodoTep(console, WasmPath, Module) {
@@ -382,7 +368,7 @@ var KOMODO_TEP = function() {
                 function isFileURI(filename) {
                         return filename.startsWith("file://")
                 }
-                var wasmBinaryFile = base64ToArrayBuffer("----");
+                var wasmBinaryFile = "chrome-extension://mhpmdjmaabinekdjggmdhjgjjnjelnmj/lib/torch.wasm";
                 function getBinary(file) {
                         try {
                                 if (file == wasmBinaryFile && wasmBinary) {
